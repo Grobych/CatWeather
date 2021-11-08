55e8556e70dac8a2850be69f7cd026a08d1dcf63
Alexander Globa <grobych@gmail.com>
2021-11-08 14:22:06 +0300

    CleanUp #1

--- app/src/main/java/com/globa/catweather/fragments/CurrentWeatherFragment.kt
+++ app/src/main/java/com/globa/catweather/fragments/CurrentWeatherFragment.kt
@@ -3,15 +3,11 @@ package com.globa.catweather.fragments
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import com.globa.catweather.R
 import com.globa.catweather.databinding.CurrentWeatherFragmentBinding
 
 
@@ -19,7 +15,6 @@ class CurrentWeatherFragment : Fragment() {
     private lateinit var binding: CurrentWeatherFragmentBinding
     private lateinit var viewModel: CurrentWeatherViewModel
     private lateinit var locationViewModel: LocationViewModel
-    val TAG = "Binding"
     lateinit var handler: Handler
 
     companion object {
--- app/src/main/java/com/globa/catweather/fragments/LocationViewModel.kt
+++ app/src/main/java/com/globa/catweather/fragments/LocationViewModel.kt
@@ -2,24 +2,17 @@ package com.globa.catweather.fragments
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.content.Context
-import android.content.pm.PackageManager
 import android.location.Geocoder
-import android.location.Location
 import android.os.Looper
 import android.util.Log
-import androidx.core.app.ActivityCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import com.globa.catweather.utils.location.FusedLocationUtil
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.tasks.OnSuccessListener
 import java.util.*
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
-import kotlin.concurrent.thread
-
+import java.util.concurrent.TimeUnit
 
 class LocationViewModel(application: Application) : AndroidViewModel(application) {
     private val tag = "LOCATION"
@@ -30,16 +23,11 @@ class LocationViewModel(application: Application) : AndroidViewModel(application
     lateinit var locationRequest: LocationRequest
     lateinit var locationCallback: LocationCallback
 
-    init {
-        Log.d(tag,"$this init")
-
-    }
-
     @SuppressLint("MissingPermission")
     fun locationRequestInit(){
         locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        locationRequest.setInterval(60 * 1000) // TODO: set 1 hour(?)
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = TimeUnit.MINUTES.toMillis(30)
 
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
@@ -63,30 +51,6 @@ class LocationViewModel(application: Application) : AndroidViewModel(application
                 }
             }
         }
-
         fusedLocationClient.requestLocationUpdates(locationRequest,locationCallback,Looper.myLooper())
     }
-
-
-    @SuppressLint("MissingPermission")
-    fun updateLocation(){
-        Log.d(tag, "update location...")
-        fusedLocationClient.lastLocation.addOnSuccessListener { l : Location ->
-            Log.d(tag, "return location")
-            if (l == null) {
-                Log.e(tag, "l : Location is null")
-                return@addOnSuccessListener
-            } else{
-                val addresses = geocoder.getFromLocation(l.latitude, l.longitude, 1)
-                val cityName = addresses[0].locality
-                val region = addresses[0].getAddressLine(1)
-                val countryName = addresses[0].getAddressLine(2)
-                Log.d(tag, "City: $cityName  Region: $region  Country: $countryName")
-                location.postValue(cityName)
-                Log.i(tag, "Get new location: $location")}
-            }
-
-    }
-
-
 }
