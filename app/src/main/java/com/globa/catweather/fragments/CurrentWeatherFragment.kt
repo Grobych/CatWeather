3292e3b9835452753f84bf2cbc1ff1a155440cba
Alexander Globa <grobych@gmail.com>
2021-11-09 15:40:24 +0300

    add weather code listener, who changing weather image

--- app/src/main/java/com/globa/catweather/fragments/CurrentWeatherFragment.kt
+++ app/src/main/java/com/globa/catweather/fragments/CurrentWeatherFragment.kt
@@ -1,5 +1,8 @@
 package com.globa.catweather.fragments
 
+import android.content.res.Resources
+import android.content.res.loader.ResourcesProvider
+import android.graphics.drawable.Drawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.os.Handler
@@ -7,7 +10,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import com.android.volley.toolbox.ImageLoader
+import com.globa.catweather.R
 import com.globa.catweather.databinding.CurrentWeatherFragmentBinding
 
 
@@ -52,6 +58,7 @@ class CurrentWeatherFragment : Fragment() {
         viewModel.windDirection.observe(viewLifecycleOwner, Observer { updatedWindDirection -> binding.windDirectionTextView.text = updatedWindDirection })
         viewModel.windSpeed.observe(viewLifecycleOwner, Observer { updatedWindSpeed -> binding.windSpeedTextView.text = updatedWindSpeed.toString() })
         viewModel.feelsLike.observe(viewLifecycleOwner, Observer { updatedFeelsLike -> binding.feelsLikeTempTextView.text = updatedFeelsLike.toString() + "° C" }) //TODO: rewrite with degree constant
+        viewModel.code.observe(viewLifecycleOwner, Observer { updatedCode -> updateImage(updatedCode) })
     }
     private fun locationChangedObserver(){
         locationViewModel.location.observe(viewLifecycleOwner, Observer { updatedLocation -> viewModel.updateWeather(this.requireContext(),updatedLocation) })
@@ -63,5 +70,24 @@ class CurrentWeatherFragment : Fragment() {
             binding.weatherRefreshLayout.isRefreshing = false
         }
     }
+
+    private fun updateImage(code : Int){ // TODO: Rewrite with weather code table
+        val drawable : Drawable? = when(code){
+            1000 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.sunny)
+            1003 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.partly_cloudy)
+            1006 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.cloudy)
+            1009 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.overcast)
+            1030 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.mist)
+            1117 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.blizzard)
+            1135 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.fog)
+            1180, 1183 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.light_rain)
+            1186, 1189 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.moderate_rain)
+            1192, 1195 -> ContextCompat.getDrawable(this.requireContext(), R.drawable.heavy_rain)
+            else -> ContextCompat.getDrawable(this.requireContext(), R.drawable.cat_weather_test)
+        }
+        binding.currentWeatherImage.setImageDrawable(drawable)
+    }
+
+
 
 }
