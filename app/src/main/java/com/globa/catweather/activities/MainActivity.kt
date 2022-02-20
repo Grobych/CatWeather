193b8b42a60bec45f3f459897e952eb8e29de0c6
Alexander Globa <grobych@gmail.com>
2022-02-20 16:28:24 +0300

    clean up code 4

--- app/src/main/java/com/globa/catweather/activities/MainActivity.kt
+++ app/src/main/java/com/globa/catweather/activities/MainActivity.kt
@@ -1,26 +1,17 @@
 package com.globa.catweather.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.globa.catweather.interfaces.ClickInterface
 import com.globa.catweather.R
 import com.globa.catweather.adapters.ViewPagerAdapter
-import com.globa.catweather.fragments.CurrentWeatherFragment
-import com.globa.catweather.fragments.DetailWeatherFragment
-import com.globa.catweather.fragments.ForecastWeatherFragment
 import com.globa.catweather.interfaces.UpdateInterface
-import com.globa.catweather.utils.FragmentSelector
 import com.globa.catweather.viewmodels.MainActivityViewModel
 
-class MainActivity : ClickInterface, FragmentActivity() {
-
-    private lateinit var fragmentContainerView: FragmentContainerView
+class MainActivity : FragmentActivity() {
 
     private lateinit var mainRefreshLayout : SwipeRefreshLayout
     private lateinit var viewModel : MainActivityViewModel
@@ -33,16 +24,6 @@ class MainActivity : ClickInterface, FragmentActivity() {
         setContentView(R.layout.activity_main)
 
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
-        if ( savedInstanceState == null){
-            viewModel.init(this)
-//            supportFragmentManager.commit {
-//                setReorderingAllowed(true)
-//                add(R.id.currentWeatherFragmentContainerView,viewModel.fragmentSelector.getCurrent())
-            }
-//        } else{
-//            viewModel.setInterface(this)
-//        }
-
         mainRefreshLayout = findViewById(R.id.mainRefreshLayout)
         refreshSwipeListener()
 
@@ -50,11 +31,6 @@ class MainActivity : ClickInterface, FragmentActivity() {
         viewPager = findViewById(R.id.mainViewPager)
         viewPager.adapter = adapter
 
-    }
-
-    override fun clicked(direction : ClickInterface.Direction) {
-        Log.d("SWIPE", "$direction ")
-//        changeFragment(direction)
     }
 
         private fun refreshSwipeListener(){
