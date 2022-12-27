9c1f660d8f3f58d48d619e2a66fcea37325c5360
a.globa <a.globa@intetics.com>
2022-12-27 23:32:15 +0300

    add font

--- app/src/main/java/com/globa/catweather/ui/constants/TextStyle.kt
+++ app/src/main/java/com/globa/catweather/ui/constants/TextStyle.kt
@@ -2,15 +2,30 @@ package com.globa.catweather.ui.constants
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.globa.catweather.R
+
+val manropeFamily = FontFamily(
+    Font(R.font.manrope_light, FontWeight.Light),
+    Font(R.font.manrope_regular, FontWeight.Normal),
+    Font(R.font.manrope_medium, FontWeight.Medium),
+    Font(R.font.manrope_bold, FontWeight.Bold)
+)
 
 val DefaultWhiteText = TextStyle(
     color = Color.White,
-    fontSize = 14.sp
+    fontSize = 14.sp,
+    fontFamily = manropeFamily,
+    fontWeight = FontWeight.Normal
 )
 
 val BigWhiteText = TextStyle(
     color = Color.White,
-    fontSize = 64.sp
+    fontSize = 64.sp,
+    fontFamily = manropeFamily,
+    fontWeight = FontWeight.Normal
 )
 
