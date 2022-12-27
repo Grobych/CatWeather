9a5eed2abdbd8bfe8028aa2dfd0b2dad83708de7
a.globa <a.globa@intetics.com>
2022-12-27 23:51:50 +0300

    fix align

--- app/src/main/java/com/globa/catweather/ui/current/CurrentWeatherScreen.kt
+++ app/src/main/java/com/globa/catweather/ui/current/CurrentWeatherScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -12,7 +14,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -75,18 +76,19 @@ fun MainInfoBlockDone(
         )
     ) {
         Column(
-            Modifier.padding(
-                start = 45.dp,
-                top = 90.dp
-            )
+            Modifier
+                .padding(
+                    start = 45.dp,
+                    top = 90.dp
+                )
+                .height(140.dp)
         ) {
             Text(
                 text = state.temperature.toString(),
                 style = BigWhiteText)
             Text(
                 text = stringResource(id = R.string.feels_like, state.feelsLikeTemperature),
-                Modifier.fillMaxWidth(0.45f), //will fix this
-                textAlign = TextAlign.Center,
+                Modifier.align(CenterHorizontally),
                 style = DefaultWhiteText
             )
         }
@@ -97,12 +99,14 @@ fun MainInfoBlockDone(
                     top = 90.dp
                 )
         ) {
-            Row() {
+            Row(
+                Modifier.height(20.dp)
+            ) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.ic_cloud),
                     contentDescription = null,
                     tint = Color.White,
-                    modifier = Modifier.padding(end = 12.dp)
+                    modifier = Modifier.padding(end = 12.dp).align(CenterVertically)
                 )
                 Text(
                     text = state.weatherState,
@@ -110,12 +114,16 @@ fun MainInfoBlockDone(
                 )
             }
 
-            Row(Modifier.padding(top = 20.dp, bottom = 20.dp)) {
+            Row(
+                Modifier
+                    .padding(top = 20.dp, bottom = 20.dp)
+                    .height(20.dp)
+            ) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.ic_wind),
                     contentDescription = null,
                     tint = Color.White,
-                    modifier = Modifier.padding(end = 12.dp)
+                    modifier = Modifier.padding(end = 12.dp).align(CenterVertically)
                 )
                 Text(
                     text = stringResource(id = R.string.wind_template, state.windSpeed),
@@ -123,12 +131,14 @@ fun MainInfoBlockDone(
                 )
             }
 
-            Row {
+            Row(
+                Modifier.height(20.dp)
+            ) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.ic_umbrella),
                     contentDescription = null,
                     tint = Color.White,
-                    modifier = Modifier.padding(end = 12.dp)
+                    modifier = Modifier.padding(end = 12.dp).align(CenterVertically)
                 )
                 Text(
                     text = stringResource(id = R.string.humidity_template, state.humidity),
